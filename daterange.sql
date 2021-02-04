61a8c77b94d1a16294ad49e3b77ce798b54d5cac
Tyler Jewell <jewelltyler15@gmail.com>
2021-02-04 11:45:37 -0500

    sql

--- daterange.sql
+++ daterange.sql
@@ -1,4 +1,7 @@
+declare @lowdate datetime = '2017-10-01';
+declare @highdate datetime = '2017-12-31 23:59:59';
+
 select *
 from orders
-where date >= '2017-10-01' and date <= '2017-12-31 23:59:59'
+where date >= @lowdate and date <= @highdate
 -- last 3 months orders 
